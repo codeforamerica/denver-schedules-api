9a7e385c1e8e96244b8fa7562deb629d91907b2a
Becky Boone <becky.sue.boone@gmail.com>
2014-05-13 15:24:03 -0700

    Add schedule list route

--- Schedules.API/Modules/SchedulesModule.cs
+++ Schedules.API/Modules/SchedulesModule.cs
@@ -4,6 +4,57 @@ public class SchedulesModule : NancyModule
 {
 	public SchedulesModule()
 	{
-		Get["/"] = parameters => "Hello Worlds";
+		Get["/schedules"] = _ =>
+		{
+			return @"{
+			title: 'City Holidays',
+			events: [
+				{
+					desc: 'New Years Day',
+					day: 'Wednesday',
+					startdate: '01.01.2014'
+				},
+				{
+					desc: 'Martin Luther King Day',
+					day: 'Monday',
+					startdate: '01.20.2014'
+				},
+				{
+					desc: 'President's Day',
+					day: 'Monday',
+					startdate: '02.27.2014'
+				},
+				{
+					desc: 'Cesar Chavez Day',
+					day: 'Monday',
+					startdate: '03.31.2014'
+				},
+				{
+					desc: 'Memorial Day',
+					day: 'Monday',
+					startdate: '05.26.2014'
+				},
+				{
+					desc: 'Independence Day',
+					day: 'Friday',
+					startdate: '07.04.2014'
+				},
+				{
+					desc: 'Labor Day',
+					day: 'Monday',
+					startdate: '11.01.2014'
+				},
+				{
+					desc: 'Thanksgiving Day',
+					day: 'Thursday',
+					startdate: '11.27.2014'
+				},
+				{
+					desc: 'Christmas Day',
+					day: 'Monday',
+					startdate: '12.25.2014'
+				}
+		]";
+		};
 	}
 }
