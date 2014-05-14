01c53955ffe2fc512d3f599264fe736d756ac50e
Becky Boone <becky.sue.boone@gmail.com>
2014-05-13 21:02:22 -0700

    Format json for js conventions
    Keep c# naming conventions in backend, but format for client side.

---
+++ Schedules.API/CustomJsonSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Schedules.API
+{
+	public class CustomJsonSerializer : JsonSerializer
+	{
+		public CustomJsonSerializer ()
+		{
+			ContractResolver = new CamelCasePropertyNamesContractResolver();
+		}
+	}
+}
+
