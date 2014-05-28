cc56a90611b2ec3213fc6a139783fd387a868e85
Becky Boone <becky.sue.boone@gmail.com>
2014-05-27 22:10:23 -0700

    Add status action
    Fake data but real route with tests

---
+++ Schedules.API.Tests/Modules/StatusTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using Nancy.Testing;
+
+namespace Schedules.API.Tests
+{
+  [TestFixture ()]
+  public class StatusTest
+	{
+    Browser browser;
+
+    [SetUp]
+    public void SetUp()
+    {
+      browser = new Browser(new CustomBootstrapper());
+    }
+
+    [Test ()]
+    public void StatusShouldReturnOK()
+    {
+      var response = browser.Get("/status", with => with.HttpRequest());
+      Assert.AreEqual(Nancy.HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Test ()]
+    public void StatusShouldReturnJson()
+    {
+      var response = browser.Get("/status", with => with.HttpRequest());
+      Assert.AreEqual ("application/json; charset=utf-8", response.ContentType);
+    }
+
+
+  }
+}
+
---
+++ Schedules.API/Modules/StatusModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Nancy;
+using Schedules.API.Extensions;
+
+public class StatusModule : NancyModule
+{
+	public StatusModule ()
+	{
+		// Engine Light App Monitoring - http://engine-light.codeforamerica.org/
+		Get ["/status"] = _ => {
+
+			var status = new {
+				Status = "ok",
+        Updated = DateTime.Now.ToUnixTimestamp(),
+				Dependenceies = new string[0],
+				Resources = new {}
+			};
+
+			return Response.AsJson (status);
+		};
+	}
+}
+
