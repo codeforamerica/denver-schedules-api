f2e33c4be2862e629f01453f8b301069e8897d84
Becky Boone <becky.sue.boone@gmail.com>
2014-05-27 21:31:23 -0700

    Create Unix Timestamp
    Timestamp is required for cfa app monitoring (using engine light)

---
+++ Schedules.API.Tests/Extensions/UnixTimeStampTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Schedules.API.Extensions;
+using NUnit.Framework;
+
+namespace Schedules.API.Tests
+{
+	public class UnixTimeStampTest
+	{
+		int timeStamp = 1401250115;
+		DateTime theDate = new DateTime(2014, 5, 28, 4, 8, 35, DateTimeKind.Utc);
+
+		[Test ()]
+		public void DateShouldConvertToUnixTimestamp()
+		{
+			var result = theDate.ToUnixTimestamp();
+			Assert.That(result, Is.EqualTo(timeStamp));
+		}
+
+		[Test ()]
+		public void UnixTimestampShouldConvertToDateTime()
+		{
+			var result = timeStamp.ToDateTime();
+			Assert.That(result, Is.EqualTo(theDate));
+		}
+	}
+}
+
