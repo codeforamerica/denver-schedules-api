a3a4565ea1f748059be56925f90fd5c200ddbfb2
Becky Boone <becky.sue.boone@gmail.com>
2014-06-13 15:59:42 -0800

    Move holidays out of controller
    Hard code data in repostory for now

--- Schedules.API/Repositories/SchedulesRepository.cs
+++ Schedules.API/Repositories/SchedulesRepository.cs
@@ -8,6 +8,68 @@ namespace Schedules.API.Repositories
     public SchedulesRepository ()
     {
     }
+
+    public object GetHolidays() {
+      return new {
+        Title = "City Holidays",
+        Events = new [] {
+          new
+          {
+            Desc = "New Years Day",
+            Day = "Wednesday",
+            StartDate = "01.01.2014"
+          },
+          new
+          {
+            Desc = "Martin Luther King Day",
+            Day = "Monday",
+            StartDate = "01.20.2014"
+          },
+          new
+          {
+            Desc = "President's Day",
+            Day = "Monday",
+            StartDate = "02.27.2014"
+          },
+          new
+          {
+            Desc = "Cesar Chavez Day",
+            Day = "Monday",
+            StartDate = "03.31.2014"
+          },
+          new
+          {
+            Desc = "Memorial Day",
+            Day = "Monday",
+            StartDate = "05.26.2014"
+          },
+          new
+          {
+            Desc = "Independence Day",
+            Day = "Friday",
+            StartDate = "07.04.2014"
+          },
+          new
+          {
+            Desc = "Labor Day",
+            Day = "Monday",
+            StartDate = "11.01.2014"
+          },
+          new
+          {
+            Desc = "Thanksgiving Day",
+            Day = "Thursday",
+            StartDate = "11.27.2014"
+          },
+          new
+          {
+            Desc = "Christmas Day",
+            Day = "Monday",
+            StartDate = "12.25.2014"
+          }
+        }
+      };
+    }
   }
 }
 
