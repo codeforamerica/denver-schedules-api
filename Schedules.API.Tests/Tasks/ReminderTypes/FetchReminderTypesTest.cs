7189e8efc52374870114b7cc5484c1ed9b89b691
Becky Boone <becky.sue.boone@gmail.com>
2014-07-21 11:48:31 -0700

    Shim for db tests
    Add a default for postgres if can't find db in config. And it can't find the db in the config, as it can't find the config. Haven't figured out how to properly link it from the main project.

--- Schedules.API.Tests/Tasks/ReminderTypes/FetchReminderTypesTest.cs
+++ Schedules.API.Tests/Tasks/ReminderTypes/FetchReminderTypesTest.cs
@@ -21,23 +21,23 @@ namespace Schedules.API.Tests.Tasks.ReminderTypes
       reminderTypes = fetchReminderTypes.Out.ReminderTypes;
     }
 
-//    [Test]
-//    public void ShouldFetchSMSReminderType() {
-//      var email = reminderTypes.Find (r => r.Name == "email");
-//      Assert.That (email, Is.Not.Null);
-//    }
-//
-//    [Test]
-//    public void ShouldFetchEmailReminderType() {
-//      var sms = reminderTypes.Find (r => r.Name == "sms");
-//      Assert.That (sms, Is.Not.Null);
-//    }
-//
-//    [Test]
-//    public void ShouldFetchYoReminderType() {
-//      var yo = reminderTypes.Find (r => r.Name == "yo");
-//      Assert.That (yo, Is.Not.Null);
-//    }
+    [Test]
+    public void ShouldFetchSMSReminderType() {
+      var email = reminderTypes.Find (r => r.Name == "email");
+      Assert.That (email, Is.Not.Null);
+    }
+
+    [Test]
+    public void ShouldFetchEmailReminderType() {
+      var sms = reminderTypes.Find (r => r.Name == "sms");
+      Assert.That (sms, Is.Not.Null);
+    }
+
+    [Test]
+    public void ShouldFetchYoReminderType() {
+      var yo = reminderTypes.Find (r => r.Name == "yo");
+      Assert.That (yo, Is.Not.Null);
+    }
   }
 }
 
