4ef5503006016f6b9fb77be2ce0ab90b9ddbafda
Becky Boone <becky.sue.boone@gmail.com>
2014-08-04 17:26:13 -0700

    Fetch a specific reminder type

---
+++ Schedules.API.Tests/Tasks/ReminderTypes/FetchReminderTypeTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Simpler;
+using Schedules.API.Tasks;
+
+namespace Schedules.API.Tests
+{
+  [TestFixture]
+  public class FetchReminderTypeTest
+  {
+    FetchReminderType fetchReminderType;
+
+    [TestFixtureSetUp]
+    public void SetUp(){
+      fetchReminderType = Task.New<FetchReminderType>();
+    }
+
+    [Test]
+    public void ShouldFetchSMSReminderType() {
+      CheckForReminderType("sms");
+    }
+
+    [Test]
+    public void ShouldFetchEmailReminderType() {
+      CheckForReminderType("email");
+    }
+
+    [Test]
+    public void ShouldFetchYoReminderType() {
+      CheckForReminderType("yo");
+    }
+
+    private void CheckForReminderType (string type)
+    {
+      fetchReminderType.In.ReminderTypeName = type;
+      fetchReminderType.Execute();
+      Assert.That(fetchReminderType.Out.ReminderType.Name, Is.EqualTo(type));
+    }
+  }
+}
+
