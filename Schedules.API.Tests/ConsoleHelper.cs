72d39fae501db1b141aa9e02e90d3bedeeb05f67
Becky Boone <becky.sue.boone@gmail.com>
2014-08-18 15:43:45 -0700

    Move console output method somewhere for reuse

---
+++ Schedules.API.Tests/ConsoleHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Schedules.API.Tests
+{
+  public static class ConsoleHelper
+  {
+    public static string CaptureOutput (Action execute)
+    {
+      using (var sw = new StringWriter ()) {
+        var previousOut = Console.Out;
+        Console.SetOut(sw);
+        execute();
+        Console.SetOut(previousOut);
+        return sw.ToString();
+      }
+    }
+  }
+}
+
