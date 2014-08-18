25cb603deab5f005c496c944d05334c113c55b44
Greg Scott <i@gregoryjscott.com>
2014-08-18 16:42:57 -0500

    Add test that checks for env variables

---
+++ Schedules.API.Tests/SystemTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Centroid;
+
+namespace Schedules.API.Tests
+{
+  [TestFixture]
+  public class SystemTests
+  {
+    [Test]
+    public void CheckThatEnvironmentVariablesExist()
+    {
+      dynamic config = Config.FromFile("config.json");
+      foreach (var variable in config.variables) {
+        var value = Environment.GetEnvironmentVariable(variable);
+        Assert.That(!String.IsNullOrEmpty(value), String.Format("{0} does not have a value.", variable));
+      }
+    }
+  }
+}
