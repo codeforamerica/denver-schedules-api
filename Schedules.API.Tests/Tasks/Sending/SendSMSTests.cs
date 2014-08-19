b7b091dcdf22a198e1b97ecf48214ff6badf6832
Becky Boone <becky.sue.boone@gmail.com>
2014-08-18 17:27:40 -0700

    Add sms reminders tasks.

---
+++ Schedules.API.Tests/Tasks/Sending/SendSMSTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+using Simpler;
+using Schedules.API.Models;
+using Schedules.API.Tasks.Sending;
+
+namespace Schedules.API.Tests
+{
+  [TestFixture]
+  public class SendSMSTests
+  {// https://www.twilio.com/docs/api/rest/test-credentials
+
+    [Test]
+    public void ShouldSendSMSForDueReminders()
+    {
+      var reminders = new [] {
+        new Reminder {
+          Contact="15005550006",
+          Message="Please do a thing"
+        }
+      };
+
+      var sendSMS = Task.New<SendSMS>();
+      sendSMS.In.DueReminders = reminders;
+      sendSMS.Execute();
+
+      Assert.That(sendSMS.Out.Sent, Is.EqualTo(reminders.Length));
+      Assert.That(sendSMS.Out.Errors, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ShouldLogErrorWhenInternationalNumber()
+    {
+      var internationalNumber = "15005550003";
+      ShouldLogErrorWhenFails(internationalNumber);
+    }
+
+    [Test]
+    public void ShouldLogErrorWhenBlacklistedNumber()
+    {
+      var blackListedNumber = "15005550004";
+      ShouldLogErrorWhenFails(blackListedNumber);
+    }
+
+    [Test]
+    public void ShouldLogErrorWhenUnableToRoute()
+    {
+      var invalidNumber = "15005550002";
+      ShouldLogErrorWhenFails(invalidNumber);
+    }
+
+    private void ShouldLogErrorWhenFails(string numberThatFails)
+    {
+      var reminders = new [] {
+        new Reminder {
+          Contact= numberThatFails,
+          Message="Please do a thing"
+        }
+      };
+
+      var sendSMS = Task.New<SendSMS>();
+      sendSMS.In.DueReminders = reminders;
+      var logOutput = ConsoleHelper.CaptureOutput(sendSMS.Execute);
+
+      Assert.That(sendSMS.Out.Sent, Is.EqualTo(0));
+      Assert.That(sendSMS.Out.Errors, Is.EqualTo(reminders.Length));
+      Assert.That(logOutput, Contains.Substring(String.Format("SMS to {0} failed", numberThatFails)));
+    }
+  }
+}
+
