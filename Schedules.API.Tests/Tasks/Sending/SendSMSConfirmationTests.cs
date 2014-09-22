57183daa777c14756a930d49e0f2fa957e9ac918
Becky Boone <becky.sue.boone@gmail.com>
2014-09-22 10:59:11 -0700

    Add functionality to send confirmation sms

---
+++ Schedules.API.Tests/Tasks/Sending/SendSMSConfirmationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Simpler;
+using Schedules.API.Tasks.Sending;
+
+namespace Schedules.API.Tests.Tasks.Sending
+{
+  [TestFixture, Category("Reminder")]
+  public class SendSMSConfirmationTests
+  {
+    private SendSMSConfirmation sendConfirmation;
+
+    [Test]
+    public void ShouldSendConfirmation()
+    {
+      sendConfirmation = Task.New<SendSMSConfirmation>();
+      sendConfirmation.In.Contact = "15005550006";
+      sendConfirmation.Execute();
+
+      Assert.That(sendConfirmation.Out.Sent, Is.EqualTo(1));
+      Assert.That(sendConfirmation.Out.Errors, Is.EqualTo(0));
+    }
+  }
+}
+
---
+++ Schedules.API/Tasks/Sending/SendSMSConfirmation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Simpler;
+using Twilio;
+using Schedules.API.Helpers;
+
+namespace Schedules.API.Tasks.Sending
+{
+  public class SendSMSConfirmation : InOutTask<SendSMSConfirmation.Input, SendSMSConfirmation.Output>
+  {
+    private const string sidKey = "TWILIO_SID";
+    private const string tokenKey = "TWILIO_TOKEN";
+    private const string numberKey = "TWILIO_NUMBER";
+    private const string message = "You are now subscribed to street sweeping reminders on {0} near {1}. Text STOP to unsubscribe."; // TODO: Get this from the config
+
+    public class Input
+    {
+      public String Contact { get; set; }
+      public String StreetName { get; set; }
+      public String Address { get; set; }
+    }
+
+    public class Output
+    {
+      public int Sent { get; set; }
+      public int Errors { get; set; }
+    }
+
+    public override void Execute()
+    {
+      var sid = EnvironmentVariableHelper.GetEnvironmentVariable(sidKey);
+      var token = EnvironmentVariableHelper.GetEnvironmentVariable(tokenKey);
+      var number = EnvironmentVariableHelper.GetEnvironmentVariable(numberKey);
+
+      var client = new TwilioRestClient (sid, token);
+
+      var result = client.SendSmsMessage(number, In.Contact, String.Format(message, In.StreetName, In.Address));
+      var error = result.RestException;
+      if (error == null) {
+        Out.Sent++;
+      } else {
+        Out.Errors++;
+        Console.WriteLine(
+          "SMS to {0} failed with a status of {1} and reason of {2}.",
+          In.Contact,
+          error.Status,
+          error.Code + ": " + error.Message
+        );
+      }
+    }
+  }
+}
+
