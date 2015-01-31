d83be94930bdde351e2f4e6b3bf676db685efc92
Erin Hao <yunhaocs@hotmail.com>
2015-01-31 15:11:56 -0700

    add contribution page

--- Schedules.API/Tasks/Sending/SendSMS.cs
+++ Schedules.API/Tasks/Sending/SendSMS.cs
@@ -13,19 +13,25 @@ namespace Schedules.API.Tasks.Sending
 
     public override void Execute ()
     {
-      var sid = EnvironmentVariableHelper.GetEnvironmentVariable(sidKey);
-      var token = EnvironmentVariableHelper.GetEnvironmentVariable(tokenKey);
-      var number = EnvironmentVariableHelper.GetEnvironmentVariable(numberKey);
+      //var sid = EnvironmentVariableHelper.GetEnvironmentVariable(sidKey);
+      //var token = EnvironmentVariableHelper.GetEnvironmentVariable(tokenKey);
+      //var number = EnvironmentVariableHelper.GetEnvironmentVariable(numberKey);
+
+
+      string sid = "ACf6fc78c90acbedbca9765990760d84fb"; 
+      string token = "e44e41839bf11f442be4d0a642f4c86c"; 
+      string number = "+19709991126";
 
       var client = new TwilioRestClient (sid, token);
 
       foreach (var reminder in In.DueReminders) 
       {
         var contact = "+" + reminder.Contact;
-        var result = client.SendSmsMessage(number, contact, reminder.Message);
+        var result = client.SendSmsMessage(number, contact , reminder.Message);
         var error = result.RestException;
         if (error == null) {
           Out.Sent++;
+          Console.WriteLine("Sent!");
         } else {
           Out.Errors++;
           Console.WriteLine(
